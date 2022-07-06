bc5661d1702d78904faf56858ce6ac24d8a568b0
Diana Gelinas <dianablythegelinas@gmail.com>
2022-07-06 15:24:10 -0600

    initial commit

---
+++ elp.js
@@ -0,0 +1,12 @@
+
+console.log('cats are fun');
+let header = document.getElementById("click");
+const potato = "french fries";
+
+header.addEventListener("mouseover", function () {
+    header.style.color = "blue";
+});
+
+header.addEventListener("mouseout", function () {
+    header.style.color = "lightgreen";
+});
